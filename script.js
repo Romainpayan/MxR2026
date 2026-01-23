abce6502608381d940ea9a2930f97cacdcad4552
Romainpayan <81319823+Romainpayan@users.noreply.github.com>
2026-01-23 15:51:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,25 +2,37 @@ const header = document.getElementById("header");
 const env = document.getElementById("envelope");
 const card = document.getElementById("card");
 
+let isIntroFinished = false;
 let isAnimationComplete = false;
 
-env.addEventListener("click", () => {
-    if (isAnimationComplete) return;
+// SÉQUENCE D'INTRODUCTION
+window.addEventListener('load', () => {
+    // On attend 3.1 secondes avant d'afficher le texte noir
+    setTimeout(() => {
+        header.style.opacity = "1";
+        isIntroFinished = true;
+    }, 3100);
+});
 
-    // 1. Descente (plus haute maintenant)
-    env.classList.add("is-descended");
+
+// SÉQUENCE AU CLIC
+env.addEventListener("click", () => {
+    if (!isIntroFinished || isAnimationComplete) return;
+
+    // 1. DÉZOOME VIF (0.5s)
+    env.classList.add("is-active");
     header.style.opacity = "0";
 
-    // 2. Ouverture
+    // 2. OUVERTURE (Démarre à 0.5s)
     setTimeout(() => {
         env.classList.add("is-opened");
-    }, 1000);
+    }, 500);
 
-    // 3. Bounce de la carte
+    // 3. REBOND DE LA CARTE (Délai total 1.2s)
     setTimeout(() => {
         card.classList.add("is-visible");
         isAnimationComplete = true;
-    }, 1600);
+    }, 1200);
 });
 
 window.addEventListener("scroll", () => {
